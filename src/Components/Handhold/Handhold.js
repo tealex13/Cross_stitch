c227b08141dade44b99e472d94ae6d3ccbf71ee9
Alex.Avila@SISU <alex.avila@sisudevices.com>
2022-06-16 09:30:46 -0500

    Changed handhold to class, to hopefully make it so I can get positon

--- src/Components/Handhold/Handhold.js
+++ src/Components/Handhold/Handhold.js
@@ -1,12 +1,46 @@
+import React from 'react';
 import	'./Handhold.css';
+import { useRef, useState, useEffect } from "react";
 
-function renderHandhold(handholdData) {
-	const id = handholdData;
-	return(
-		<button className = 'Handhold'  key = {id}>
-				{id}
+
+
+class Handhold extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {id: 0}
+	}
+	// const boxRef = useRef();
+
+	//   // X
+	// const [x, setX] = useState();
+
+	// // Y
+	// const [y, setY] = useState();
+	// const getPosition = () => {
+	// // const x = boxRef.current.offsetLeft;
+	// const x = 2;
+	// setX(x);
+
+	// // const y = boxRef.current.offsetTop;
+	// const y = 3;
+	// setY(y);
+	// 	};
+
+	// useEffect(() => {
+	//     getPosition();
+	//   }, []);
+
+
+	 
+
+	render(){
+		return(
+		<button className = 'Handhold'  key = {this.state.id}>
+				{this.props.id}
 		</button>
-		);
+		)
+		};
 }
 
-export {renderHandhold}
+export {Handhold}
--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,12 +1,13 @@
 import	'./Row.css';
-import {renderHandhold} from '../Handhold/Handhold.js'
+import {Handhold} from '../Handhold/Handhold.js'
 
 function renderRow(rowData){
 	// console.log( 'row data ' + rowData);	
 	const row = [];
 	const id = rowData[0];
     for (let i = 0; i < rowData.length; i++){
-      row.push(renderHandhold(rowData[i]));
+      row.push(<Handhold id = {rowData[i]}/>)
+      // row.push(<div>hello</div>);
     }
 
 	return(
